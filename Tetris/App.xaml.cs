a33bae9de608d258d3f83706ac67dd2fa4747134
Taka414 <yyyymmddhhmmsssss@gmail.com>
2015-03-25 22:10:24 +0900

    名前空間の名称を変更

--- Tetris/App.xaml.cs
+++ Tetris/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfApplication1
+namespace Tetris
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
--- Tetris/TerorisMainWindow.xaml.cs
+++ Tetris/TerorisMainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
-namespace WpfApplication1
+namespace Tetris
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
